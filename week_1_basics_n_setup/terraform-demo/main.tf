5bc134f098b0bb877b4e0cb2ceca2abe6eff2407
MyoThet <mr.myothet@gmail.com>
2025-01-23 00:44:58 +0700

    terraform variables

--- week_1_basics_n_setup/terraform-demo/main.tf
+++ week_1_basics_n_setup/terraform-demo/main.tf
@@ -8,14 +8,14 @@ terraform {
 }
 
 provider "google" {
-  credentials = "./keys/my-creds.json"
-  project     = "mt-terraform-demo"
-  region      = "us-central1"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
   name          = "mt-terraform-demo-terra-bucket"
-  location      = "US"
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -26,4 +26,10 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+
 }
