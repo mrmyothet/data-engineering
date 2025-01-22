32405c137fd3b193375de54a446122f3352f6810
MyoThet <mr.myothet@gmail.com>
2025-01-22 23:22:04 +0700

    terraform - creating main.tf

---
+++ week_1_basics_n_setup/terraform-demo/Notes.md
@@ -0,0 +1,5 @@
+```bash
+export GOOGLE_CREDENTIALS='/Users/macos/my-creds.json'
+
+echo $GOOGLE_CREDENTIALS
+```
---
+++ week_1_basics_n_setup/terraform-demo/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.17.0"
+    }
+  }
+}
+
+provider "google" {  
+  credentials = "./keys/my-creds.json"
+  project = "terraform-demo"
+  region = "us-central1"
+}
