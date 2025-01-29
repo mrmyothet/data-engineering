133f7e65f9eb855909ceedcaccd0f8c5a06bcda1
Myo Thet <mr.myothet@gmail.com>
2025-01-29 18:33:36 +0700

    gettting started with Kestra - outputs

--- workflow_orchestration/getting_started/scripts/api_example.py
+++ workflow_orchestration/getting_started/scripts/api_example.py
@@ -1,5 +1,7 @@
 import requests
+from kestra import Kestra
 
 r = requests.get("https://api.github.com/repos/kestra-io/kestra")
 gh_stars = r.json()["stargazers_count"]
-print(gh_stars)
+
+Kestra.outputs({'gh_stars': gh_stars})
