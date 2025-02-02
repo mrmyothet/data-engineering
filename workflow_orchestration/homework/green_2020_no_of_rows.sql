b6321dcf6e212a229dfdcb5f5ea896287dfd20d4
Myo Thet <mr.myothet@gmail.com>
2025-02-02 20:14:20 +0700

    homework - question 4 - number of rows - green 2020

---
+++ workflow_orchestration/homework/green_2020_no_of_rows.sql
@@ -0,0 +1,17 @@
+-- select count(1) from green_tripdata_staging;
+select count(1) from green_tripdata;
+
+-- 447770
+-- 398632
+-- 223406
+-- 35612
+-- 57360
+-- 63109
+-- 72257
+-- 81063
+-- 87987
+-- 95120
+-- 88605
+-- 83130
+
+-- 1734051
