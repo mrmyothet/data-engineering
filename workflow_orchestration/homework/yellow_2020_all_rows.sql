c3d0b22e0beccafacc8c15894eb394c2b7be4ad4
Myo Thet <mr.myothet@gmail.com>
2025-02-02 19:58:21 +0700

    homework - question 3 - number of rows - yellow 2020

---
+++ workflow_orchestration/homework/yellow_2020_all_rows.sql
@@ -0,0 +1,17 @@
+-- select count(*) from yellow_tripdata_staging;
+
+select count(*) from yellow_tripdata;
+
+-- 6405008
+-- 6299354
+-- 3007292
+-- 237993
+-- 348371
+-- 549760
+-- 800412
+-- 1007284
+-- 1341012
+-- 1681131
+-- 1461897
+
+-- 24648499
