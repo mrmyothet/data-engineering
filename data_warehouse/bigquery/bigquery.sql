be18b5ebd08f61a916665bdc413f690ad99e6917
Myo Thet <mr.myothet@gmail.com>
2025-02-12 18:03:19 +0700

    create external table by referring to GCS path

--- data_warehouse/bigquery/bigquery.sql
+++ data_warehouse/bigquery/bigquery.sql
@@ -1,3 +1,12 @@
 SELECT station_id, name
 FROM bigquery-public-data.new_york_citibike.citibike_stations
 LIMIT 100;
+
+
+CREATE OR REPLACE EXTERNAL TABLE `ny-rides-mt.ny_taxi.external_yellow_tripdata`
+OPTIONS(
+  format = 'parquet',
+  uris = ['gs://ny_taxi_data_mt/yellow_tripdata_2019-*.parquet', 'gs://ny_taxi_data_mt/yellow_tripdata_2020-*.parquet']
+);
+
+SELECT * FROM `ny-rides-mt.ny_taxi.external_yellow_tripdata` limit 10;
